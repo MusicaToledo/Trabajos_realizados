561fadcdfd579f9c10d15b8ee768f023a113d3cb
MusicaToledo <milenafigueredo1999@gmail.com>
2026-03-24 14:48:36 -0300

    Add files via upload

---
+++ CALCULO CLAVE .py
@@ -0,0 +1,8 @@
+ingreso = (input("Ingrese clave: "))
+
+clave_real="python123"
+
+if clave_real == ingreso:
+    print("La clave es correcta")
+elif clave_real != ingreso:
+    print("La clave es incorrecta")
